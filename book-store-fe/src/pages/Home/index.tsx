f0d10e2af4b5d78bf13bd682bcf1f9f050033c6f
Slavi FE <slavidimitrov54@gmail.com>
2026-03-03 21:58:39 +0200

    add more changes to FE

--- book-store-fe/src/pages/Home/index.tsx
+++ book-store-fe/src/pages/Home/index.tsx
@@ -1,3 +1,8 @@
+import {Button} from "@mui/material";
+
 export default function Home() {
-  return <h1>Home</h1>;
+    return (<>
+        <h1>Home</h1>
+        <Button variant="outlined">Hello world</Button>
+    </>);
 }
